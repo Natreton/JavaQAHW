ee3d9f29c7ca439ad7e2d0d37b8dc9b0ef2d7c42
Natreton <natreton@abv.bg>
2020-08-10 17:41:29 +0300

    Update FourthFromL11.java

--- FourthFromL11.java
+++ FourthFromL11.java
@@ -4,17 +4,18 @@ public class FourthFromL11 {
 	
 	
 	 
-	public static int findLargest() 
-     { 
+	public static int findLargest() {
+     
 		 int[] myArr = {10, 324, 45, 90, 1000, 300, 450, 500};
          int i; 
            
          int max = myArr[0]; 
          
-         for (i = 1; i < myArr.length; i++) 
+         for (i = 1; i < myArr.length; i++) {
              if (myArr[i] > max){
                  max = myArr[i]; 
 	     }
+	 }
          return max; 
      } 
 
